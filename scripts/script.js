920f8da937cdce42e47b87d195ba2deddf03497a
emsetohughes <emma.setohughes@gmail.com>
2018-11-20 20:08:18 -0500

    google api work

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ myApp.googleKey = "AIzaSyAtUxpG10DW19jF5_OC6Q5rfT3PO5Nzmos";
 
 // get user lat and longitude
 
+
 // set to variables
 myApp.day = 20;
 myApp.month = 11;
@@ -15,17 +16,30 @@ myApp.year = 2017;
 myApp.setLatLong = function(){
     const latitude = 43.6532;
     const longitude = 79.3832;
+    myApp.getLocation(latitude, longitude);
     const units = "ca";
     const startDate = new Date(`${myApp.year}, ${myApp.month}, ${myApp.day}`).getTime() / 1000;
 
     myApp.getTemp(latitude,longitude, units, startDate);
 }
 
-myApp.getLocation = function(){
-    $.ajax({
 
-    }).then(res => {
-        console.log(res);
+const input = "toronto";
+
+myApp.getLocation = function(input){
+    // const autocomplete = new google.maps.places.Autocomplete(input);
+    $.ajax({
+        //https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY
+        url: `https://maps.googleapis.com/maps/api/place/autocomplete/jsonp?input=toronto&key=${myApp.googleKey}`,
+        dataType: 'jsonp',
+        method: 'GET',
+        data: {
+            key: myApp.googleKey,
+            input: input
+        }
+
+    }).then(googleRes => {
+        console.log(googleRes);
     })
 }
 
