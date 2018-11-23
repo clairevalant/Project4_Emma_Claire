060a1999076686e00fd56fa5ba2da6bc0b26549d
emsetohughes <emma.setohughes@gmail.com>
2018-11-22 22:05:06 -0500

    cleaned up the giant getInput function by breaking it up into asynchronous events (separate functions) still need to work on making the ajax calls all complete before printing the array

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@
 const myApp = {};
 
 // store our api keys for weather and google places
-myApp.weatherKey = "5a94fd0eec1f352b4f9876ecb51a88a9";
+myApp.weatherKey = "939526212d6d03b772f203915ea5ef22";
+
 myApp.googleKey = "AIzaSyAtUxpG10DW19jF5_OC6Q5rfT3PO5Nzmos";
 
 // tell our autoComplete input to actually Autocomplete in the specified input element "autoComplete"
@@ -24,96 +25,132 @@ google.maps.event.addListener(myApp.autoCompInput, "place_changed", function () 
 myApp.getInput = function(){
     $("form").on("submit", function(event){
         event.preventDefault();
+        myApp.collectFormData();     
+    }
+)};
 
-        // toggle temperature units depending on user choice
-        if ($("input[value=ca]:checked").val() === "ca") {
-            units = "ca";
-        } else {
-            units = "us";
-        }
-        console.log(units);
+myApp.collectFormData = function(){
+    // toggle temperature units depending on user choice
+    if ($("input[value=ca]:checked").val() === "ca") {
+        units = "ca";
+    } else {
+        units = "us";
+    }
+    console.log(units);
 
-        // get user date range / trip type (?) / unit toggle
-        myApp.startDate = new Date($("#startDate").val());
-        myApp.endDate = new Date($("#endDate").val());
-        console.log(myApp.startDate);
-        console.log(myApp.endDate);
+    // get user date range / trip type (?) / unit toggle
+    myApp.startDate = new Date($("#startDate").val());
+    myApp.endDate = new Date($("#endDate").val());
+    console.log(myApp.startDate);
+    console.log(myApp.endDate);
+
+
+    if (myApp.startDate > myApp.endDate) {
+        alert("Please enter a valid end date.");
+        // RESET END DATE CALENDAR TO DEFAULT
+    }
+
+    const sDate = myApp.startDate.getDate() + 1;
+    const sMonth = myApp.startDate.getMonth() + 1;
+    let sYear = myApp.startDate.getFullYear();
+
+
+    const eDate = myApp.endDate.getDate() + 1;
+    const eMonth = myApp.endDate.getMonth() + 1;
+    let eYear = myApp.endDate.getFullYear();
+
+
+    // change user dates to Epoch time for past five years, send to DarkSky
+    let startEpoch = new Date(`${sYear}, ${sMonth}, ${sDate}`).getTime() / 1000;
+    let endEpoch = new Date(`${eYear}, ${eMonth}, ${eDate}`).getTime() / 1000;
+
+
+    // initialize loop date to previous year
+    let current = startEpoch;
+
+    // call function to start retrieving historical data from Dark Sky API
+    myApp.getTripDuration(startEpoch, endEpoch, current);
+};     
         
-
-        if (myApp.startDate > myApp.endDate) { 
-            alert("Please enter a valid end date.");
-            // RESET END DATE CALENDAR TO DEFAULT
-        }
-
-        const sDate = myApp.startDate.getDate() + 1;
-        const sMonth = myApp.startDate.getMonth() + 1;
-        let sYear = myApp.startDate.getFullYear();
+        
+        
+myApp.getTripDuration = function (startEpoch, endEpoch, current) {
     
-        
-        const eDate = myApp.endDate.getDate() + 1;
-        const eMonth = myApp.endDate.getMonth() + 1;
-        let eYear = myApp.endDate.getFullYear();
-    
+    // initialize arrays for max and min temps for past five years on specified date
+    myApp.historyTempMin = [];
+    myApp.historyTempMax = [];
 
-        // change user dates to Epoch time for past five years, send to DarkSky
-        let startEpoch = new Date(`${sYear}, ${sMonth}, ${sDate}`).getTime() / 1000;
-        let endEpoch = new Date(`${eYear}, ${eMonth}, ${eDate}`).getTime() / 1000;
-        
+    // how many days we increment by in algorithm (longer trips: every few days)
+    let dayIncrement = 86400;
 
-        // initialize loop date to previous year
-        let current = startEpoch;
+    // set how many days we take in the trip duration
+    let numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
 
-        // initialize arrays for max and min temps for past five years on specified date
-        myApp.historyTempMin = [];
-        myApp.historyTempMax = [];
+    // handling for "0" days
+    if (numDays <= 1) {
+        numDays = 2;
+    }
+    // only look at every other day for high numDays
+    if (numDays > 7 && numDays <= 15) {
+        dayIncrement = 86400 * 2;
+        numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
+    }
+    if (numDays > 15) {
+        dayIncrement = 86400 * 3;
+        numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
+    }
 
-        // how many days we increment by in algorithm (longer trips: every few days)
-        let dayIncrement = 86400;
+    myApp.getHistoricalData(numDays, current, dayIncrement);
 
-        // set how many days we take in the trip duration
-        let numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
-
-        // handling for "0" days
-        if (numDays <= 1) {
-            numDays = 2;
-        }
-        // only look at every other day for high numDays
-        if (numDays > 7 && numDays <= 15) {
-            dayIncrement = 86400 * 2;
-            numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
-        }
-        if (numDays > 15) {
-            dayIncrement = 86400 * 3;
-            numDays = Math.floor((endEpoch - startEpoch) / dayIncrement);
-        }
-    
-        // for each day in the trip
-        for (let j = 0; j < numDays; j++) {
-            console.log(j);
-            
-            // retrieve temperature for the past five years
-            for (let i = 0; i < 5; i++) {
-                
-                myApp.getTemp(myApp.lat, myApp.lng, units, current);
-                // subract one Epoch year
-                current = current - 31536000;
-                console.log(current);
-            }
-            // add one Epoch day, add Epoch five years
-            current = current + dayIncrement + (5 * 31536000);
-        }
-        
-        console.log("TempMax",myApp.historyTempMax);
-        console.log("TempMin", myApp.historyTempMin);
-
-    });
 };
+
+myApp.getHistoricalData = function (numDays, current, dayIncrement) {
+
+   
+    // for each day in the trip
+    for (let j = 0; j < numDays; j++) {
+        console.log(j);
+
+        // retrieve temperature for the past five years
+        for (let i = 0; i < 5; i++) {
+            myApp.getTemp(myApp.lat, myApp.lng, units, current);
+            // subract one Epoch year
+            current = current - 31536000;
+            console.log(current);
+        }
+        // add one Epoch day, add Epoch five years
+        current = current + dayIncrement + (5 * 31536000);
+    };
+
+
+    myApp.outputHistoricalData();
+};
+
+myApp.outputHistoricalData = function() {
+    console.log("TempMax", myApp.historyTempMax);
+    console.log("TempMin", myApp.historyTempMin);
+    // console.log(myApp.historyTempMax[0]);
+    setTimeout(() => { console.log(myApp.historyTempMax[0]) }, 2000);
+
+    myApp.getAverageTemp();
+};
+
+myApp.getAverageTemp = function() {
+    // let summation = 0; 
+    // for (let k = 0; k < myApp.historyTempMax.length; k++) {
+    //     summation = myApp.historyTempMax[k] + summation;
+    //     console.log(summation);
+    // }
+
+    //console.log("tempMax", summation / myApp.historyTempMax.length);
+};
+
 
 // ----- Weather app API work begins here -----
 
 myApp.getTemp = function(lat, long, u, t){
-
     $.ajax({
+        
         url: `https://api.darksky.net/forecast/${myApp.weatherKey}/${lat},${long},${t}`,
         dataType: "jsonp",
         method: "GET",
@@ -122,7 +159,7 @@ myApp.getTemp = function(lat, long, u, t){
             key: myApp.weatherKey,
             units: u
         }
-    }).then(res => {
+    }).then(res => {        
         // push max and min temps for that day
         myApp.historyTempMin.push(res.daily.data[0].temperatureMin);
         myApp.historyTempMax.push(res.daily.data[0].temperatureMax);
